5d062353d385f78f5b3e6291e53b9f17b5f3064d
Yousef <47320275+yalhaizaey@users.noreply.github.com>
2022-04-06 15:59:25 +0100

    Add files via upload

---
+++ find_max/jpso_linear_model.py
@@ -0,0 +1,220 @@
+''' Python PSO implementations. Linear Model.'''
+import random
+import math
+import copy
+import sys
+import numpy
+import time
+from numpy.random import normal
+from numpy import rint
+import collections
+##------------------------------------------------------------------------------
+n=10 ##number of tasks to be allocated
+
+Max_Task_id=2 ##for differet app
+
+##new_costs = Yolo Data, PS Data, Aeneas Data
+def randomTaskArray(n):
+    taskArray = []
+    for i in range (n):
+         taskArray.append(random.randint(0,Max_Task_id))
+    return (taskArray)
+
+def uniformTaskArray (n, taskId):
+    return [taskId]*n
+
+def equalPartsTaskArray(n):
+    i=0
+    tasks = list(range(0,Max_Task_id+1))
+    taskArray=[]
+    while i<n:
+        taskArray.append(task[i%len(tasks)])
+        i = i+1
+    return taskArray
+
+new_costs = [
+            [{"m":10, "c":45}, {"m":10, "c":25}, {"m":7, "c":7}],   # pi2 (0)
+            [{"m":10, "c":45}, {"m":10, "c":25}, {"m":7, "c":7}],   # pi2 (0)
+            [{"m":10, "c":45}, {"m":10, "c":25}, {"m":7, "c":7}],   # pi2 (0)
+
+            [{"m":15, "c":20}, {"m":10, "c":10}, {"m":5, "c":3}],   # pi3 (1)
+            [{"m":15, "c":20}, {"m":10, "c":10}, {"m":5, "c":3}],   # pi3 (1)
+            [{"m":15, "c":20}, {"m":10, "c":10}, {"m":5, "c":3}],   # pi3 (1)
+            [{"m":15, "c":20}, {"m":10, "c":10}, {"m":5, "c":3}],   # pi3 (1)
+
+            [{"m":5, "c":5}  , {"m":5, "c":7}, {"m":2, "c":5}]      # pi4 (2)
+            ];
+
+#tasklookup = randomTaskArray(n)
+tasklookup = [0,0,0,2,2,2,2,1,1,1]
+# tasklookup = [1,1,1,1,1,1,1,1,1,1]
+# tasklookup = [2,2,2,2,2,2,2,2,2,2]
+print (tasklookup)
+##------Nodes Capacities Function-----------------------------------------------
+def nodes_cap(particle):
+    ''' check if a particle meets the nodes capacity constraints.
+        return True if meet the constraints.
+        return False otherwise '''
+    node_cap = [n*0.2]*3 + [n*0.2]*4 + [n*0.6]*1 ## 3nodes(RPi2), 4nodes(RPi3), 1node(RPi4)
+    particle = [int (i) for i in particle]
+    counter=collections.Counter(particle)
+    for i in (counter):
+        if (counter.get(i) > node_cap[i]):
+            return False
+    return True
+#-------New Fitness Function (y=mx+b)-------------------------------------------
+''' New fitness function based on linear observations (y=mx+b) '''
+def fitness_fun(particle):
+    ''' particle is a list - n integers - one integer per task,
+        the integer determines which _node_ this indexed task will be allocated'''
+    particle = [int (i) for i in particle]
+    fitnessVal = 0
+    ''' counter to calculate how many tasks assinged to each node in a particle'''
+    counter = collections.Counter(particle)
+    for node in counter:
+        numTasks = counter.get(node)
+        tasksonthisnode = [0]*(Max_Task_id+1)
+        #print ("on node ", node, " there are ", numTasks, " tasks");
+        for i in range(len(particle)):
+            if particle[i] == node:
+                taskId=tasklookup[i]
+                #print ("line 81 task", taskId, "is running on node", node)
+                tasksonthisnode[taskId]+=1
+        print ("tasks on thins node:", tasksonthisnode)
+    for node in counter:
+        nodeFitness=0
+        # find out how many tasks are running on that node
+        numTasks = counter.get(node)
+        tasksonthisnode = [0]*(Max_Task_id+1)
+        #print ("on node ", node, " there are ", numTasks, " tasks");
+        for i in range(len(particle)):
+            if (particle[i] == node):
+                taskId=tasklookup[i]
+                #print ("line 81 task", taskId, "is running on node", node)
+                tasksonthisnode[taskId]+=1
+        ''' call nodes_cap function to check node capacity constraints
+            if nodes constraints is False, FitnessVal is (inf)
+            if nodes constraints is True,  FirnessVal is calculated '''
+        if (nodes_cap(particle) == False):
+            nodeFitness = float('inf')
+        else:
+            time = 0
+            for taskId in range(0,Max_Task_id+1):
+                m = new_costs[node][taskId]['m']
+                c = new_costs[node][taskId]['c']
+                time += m*tasksonthisnode[taskId] + c
+            nodeFitness = time
+        if (nodeFitness > fitnessVal):
+            fitnessVal = nodeFitness
+    return fitnessVal
+##------Start PSO---------------------------------------------------------------
+''' Staring PSO Optimisation'''
+class Particle:
+  def __init__(self, fitness, dim, minx, maxx, seed):
+    self.rnd = random.Random(seed)
+
+    # initialize position of the particle with 0.0 value
+    self.position = [0.0 for i in range(dim)]
+
+    # initialize velocity of the particle with 0.0 value
+    self.velocity = [0.0 for i in range(dim)]
+
+    # initialize best particle position of the particle with 0.0 value
+    self.best_part_pos = [0.0 for i in range(dim)]
+
+    # loop dim times to calculate random position and velocity
+    # range of position and velocity is [minx, max]
+    for i in range(dim):
+      self.position[i] = ((maxx - minx) * self.rnd.random() + minx)
+      self.velocity[i] = ((maxx - minx) * self.rnd.random() + minx)
+
+    # compute fitness of particle
+    self.fitness = fitness(self.position)
+
+    # initialize best position and fitness of this particle
+    self.best_part_pos = copy.copy(self.position)
+    self.best_part_fitnessVal = self.fitness
+
+def pso(fitness, max_iter, n, dim, minx, maxx):
+  ''' Defineing PSO Hyperparameters '''
+  w = 0.729    # inertia wieght
+  c1 = 1.49445 # cognitive (particle)
+  c2 = 1.49445 # social (swarm)
+  rnd = random.Random()
+
+  # create n random particles
+  swarm = [Particle(fitness, dim, minx, maxx, i) for i in range(n)]
+
+  # compute the value of best_position and best_fitness in swarm
+  best_swarm_pos = [0.0 for i in range(dim)]
+  best_swarm_fitnessVal = sys.float_info.max # swarm best
+
+  # computer best particle of swarm and it's fitness
+  for i in range(n):
+    if swarm[i].fitness < best_swarm_fitnessVal:
+      best_swarm_fitnessVal = swarm[i].fitness
+      best_swarm_pos = copy.copy(swarm[i].position)
+
+  Iter = 0
+  while Iter < max_iter:
+    if Iter % 10 == 0 and Iter > 1:
+      print("Iter = " + str(Iter) + " best fitness = %.3f" % best_swarm_fitnessVal)
+
+    for i in range(n):
+      # compute new velocity of curr particle
+      for k in range(dim):
+        r1 = rnd.random()
+        r2 = rnd.random()
+        swarm[i].velocity[k] = (
+                               (w * swarm[i].velocity[k]) +
+                               (c1 * r1 * (swarm[i].best_part_pos[k] - swarm[i].position[k])) +
+                               (c2 * r2 * (best_swarm_pos[k] -swarm[i].position[k]))
+                               )
+      ''' compute fitness of new position '''
+      swarm[i].fitness = fitness(swarm[i].position)
+
+      # Is new position a new best for the particle?
+      if swarm[i].fitness < swarm[i].best_part_fitnessVal:
+        swarm[i].best_part_fitnessVal = swarm[i].fitness
+        swarm[i].best_part_pos = copy.copy(swarm[i].position)
+
+      # Is new position a new best overall?
+      if swarm[i].fitness < best_swarm_fitnessVal:
+        best_swarm_fitnessVal = swarm[i].fitness
+        best_swarm_pos = copy.copy(swarm[i].position)
+
+      # Added by Yousef! -- compute new position using new velocity & clip new position between minx and maxx
+      for k in range(dim):
+        swarm[i].position[k] += swarm[i].velocity[k]
+        if swarm[i].position[k] < minx:
+            swarm[i].position[k] = minx
+        elif swarm[i].position[k] > maxx:
+            swarm[i].position[k] = maxx
+    Iter += 1
+  return best_swarm_pos
+##----------------End-of-PSO ---------------------------------------------------
+''' Driver Code '''
+stime = time.time()
+print("\nBegin Particle Swarm Optimization \n")
+dim = n  #here is the number of tasks (n is the number of tasks)
+fitness = fitness_fun
+num_particles = 50
+max_iter = 100
+
+print("Goal is to minimize cost function in " + str(dim) + " variables")
+print("Setting num_particles = " + str(num_particles))
+print("Setting max_iter    = " + str(max_iter))
+print("\nStarting PSO algorithm\n")
+
+best_position = pso(fitness, max_iter, num_particles, dim, 0, 7)
+
+print("\nPSO completed\n")
+print("\nBest solution found:")
+best_position = [int (i) for i in (best_position)]
+print (best_position)
+
+fitnessVal = fitness(best_position)
+print("\nFitness of best solution = %.6f" % fitnessVal)
+print("\nEnd Particle Swarm Optimization\n")
+etime = time.time()
+print ("Time =", etime-stime)
---
+++ icfec_codes/icfec_pso.py
@@ -0,0 +1,199 @@
+# python implementation of particle swarm optimization (PSO)
+import random
+import math    # cos() for Rastrigin
+import copy    # array-copying convenience
+import sys     # max float
+import numpy
+import time
+from numpy.random import normal
+from numpy import rint
+
+
+##----------Generating Data--------------
+##randomize data;
+def prMatrix(x):
+    for row in x:
+        for val in row:
+            print(val,end=',')
+        print()
+    print()
+
+# example array of task costs on different nodes
+n= 1000 #number of tasks to be allocated
+
+# estimated extecution time tasks take on nodes. 50 means Pi2B, 40 means Pi3B, 10 means Pi4B
+x = [
+    [50]*n, [50]*n, [50]*n, [50]*n, [50]*n, [50]*n, [50]*n, [50]*n, [50]*n,
+
+    [40]*n, [40]*n, [40]*n, [40]*n, [40]*n, [40]*n, [40]*n, [40]*n, [40]*n,
+
+    [10]*n, [10]*n,
+    ]
+print (x)
+print('initial x:')
+print('----------')
+prMatrix(x)
+####### @jsinger new perturbation code for cost matrix
+# thresholds for changing costs
+COEFFICIENT_OF_VARIATION=0.5  # c.o.v. = stdev / mean = sigma/mu
+# try different values - between 0 and 1?
+for i in range(len(x)):
+    for j in range(len(x[i])):
+        mu = x[i][j]
+        sigma = COEFFICIENT_OF_VARIATION * mu
+        updated_value = int(rint(normal(mu, sigma)))
+        x[i][j] = max(0, updated_value)  # no negative costs!
+
+print('final x:')
+print('----------')
+prMatrix(x)
+
+#execution time cost
+costs = x
+
+#-------fitness functions----------------
+def fitness_fun(position):
+    particle =(position) # particle = solution
+    num_nodes = len(costs)
+    num_tasks = len(particle)
+    fitnessVal = 0.0
+    for t in range(num_tasks):
+        node = int (particle[t])
+        assert node <= num_nodes
+        fitnessVal += costs [node][t]
+    return fitnessVal
+
+#-------------------------#-------------------------
+#particle class
+class Particle:
+  def __init__(self, fitness, dim, minx, maxx, seed):
+    self.rnd = random.Random(seed)
+
+    # initialize position of the particle with 0.0 value
+    self.position = [0.0 for i in range(dim)]
+
+     # initialize velocity of the particle with 0.0 value
+    self.velocity = [0.0 for i in range(dim)]
+
+    # initialize best particle position of the particle with 0.0 value
+    self.best_part_pos = [0.0 for i in range(dim)]
+
+    # loop dim times to calculate random position and velocity
+    # range of position and velocity is [minx, max]
+    for i in range(dim):
+      self.position[i] = ((maxx - minx) * self.rnd.random() + minx)
+      self.velocity[i] = ((maxx - minx) * self.rnd.random() + minx)
+
+
+    # compute fitness of particle
+    self.fitness = fitness(self.position) # curr fitness
+
+    # initialize best position and fitness of this particle
+    self.best_part_pos = copy.copy(self.position)
+    self.best_part_fitnessVal = self.fitness # best fitness
+
+#-------------------------#-------------------------
+# particle swarm optimization function
+def pso(fitness, max_iter, n, dim, minx, maxx):
+  # hyper parameters
+  w = 0.729    # inertia
+  c1 = 1.49445 # cognitive (particle)
+  c2 = 1.49445 # social (swarm)
+  rnd = random.Random()
+
+  # create n random particles
+  swarm = [Particle(fitness, dim, minx, maxx, i) for i in range(n)]
+
+  # compute the value of best_position and best_fitness in swarm
+  best_swarm_pos = [0.0 for i in range(dim)]
+  best_swarm_fitnessVal = sys.float_info.max # swarm best
+
+  # computer best particle of swarm and it's fitness
+  for i in range(n): # check each particle
+    if swarm[i].fitness < best_swarm_fitnessVal:
+      best_swarm_fitnessVal = swarm[i].fitness
+      best_swarm_pos = copy.copy(swarm[i].position)
+
+  # main loop of pso
+  Iter = 0
+  while Iter < max_iter:
+
+    # after every 10 iterations
+    # print iteration number and best fitness value so far
+    if Iter % 10 == 0 and Iter > 1:
+      print("Iter = " + str(Iter) + " best fitness = %.3f" % best_swarm_fitnessVal)
+
+    for i in range(n): # process each particle
+
+      # compute new velocity of curr particle
+      for k in range(dim):
+        r1 = rnd.random()    # randomizations
+        r2 = rnd.random()
+
+        swarm[i].velocity[k] = (
+                                 (w * swarm[i].velocity[k]) +
+                                 (c1 * r1 * (swarm[i].best_part_pos[k] - swarm[i].position[k])) +
+                                 (c2 * r2 * (best_swarm_pos[k] -swarm[i].position[k]))
+                               )
+
+      # compute fitness of new position
+      swarm[i].fitness = fitness(swarm[i].position)
+
+      # is new position a new best for the particle?
+      if swarm[i].fitness < swarm[i].best_part_fitnessVal:
+        swarm[i].best_part_fitnessVal = swarm[i].fitness
+        swarm[i].best_part_pos = copy.copy(swarm[i].position)
+
+      # is new position a new best overall?
+      if swarm[i].fitness < best_swarm_fitnessVal:
+        best_swarm_fitnessVal = swarm[i].fitness
+        best_swarm_pos = copy.copy(swarm[i].position)
+
+
+      # Added by Yousef! -- compute new position using new velocity & clip new position between minx and maxx
+      for k in range(dim):
+        swarm[i].position[k] += swarm[i].velocity[k]
+        if swarm[i].position[k] < minx:
+            swarm[i].position[k] = minx
+        elif swarm[i].position[k] > maxx:
+            swarm[i].position[k] = maxx
+
+    # for-each particle
+    Iter += 1
+  #end_while
+  return best_swarm_pos
+# end pso
+
+#----------------------------#-------------------------#----------
+# Driver code
+import time
+stime = time.time()
+print("\nBegin Particle Swarm Optimization \n")
+dim = n  #here is the number of tasks (n is the number of tasks)
+fitness = fitness_fun
+
+print("Goal is to minimize cost function in " + str(dim) + " variables")
+
+for i in range(dim-1):
+    print("0, ", end="")
+print("0")
+
+num_particles = 50
+max_iter = 100
+
+print("Setting num_particles = " + str(num_particles))
+print("Setting max_iter    = " + str(max_iter))
+print("\nStarting PSO algorithm\n")
+
+
+best_position = pso(fitness, max_iter, num_particles, dim, 0, 19)
+
+print("\nPSO completed\n")
+print("\nBest solution found:\n")
+print(["%.6f"%best_position[k] for k in range(dim)])
+fitnessVal = fitness(best_position)
+print("\nfitness of best solution = %.6f" % fitnessVal)
+
+print("\nEnd Particle Swarm Optimization\n")
+etime = time.time()
+print ("time =", etime-stime)
